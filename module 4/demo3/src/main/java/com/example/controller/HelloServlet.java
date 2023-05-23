559b34ceb473ffa319069efc5fe6e93b8e26807c
Nam <nguyenphuongnamnphn@example.com>
2023-05-24 00:56:53 +0700

    cap nhat git

---
+++ module 4/demo3/src/main/java/com/example/controller/HelloServlet.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@Controller
+public class HelloServlet {
+
+}
