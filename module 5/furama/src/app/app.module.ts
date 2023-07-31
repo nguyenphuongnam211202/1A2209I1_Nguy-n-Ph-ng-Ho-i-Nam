9416d41ea712c0b38735d6f83872eca60ca63a89
Nam <nguyenphuongnamnphn@example.com>
2023-07-31 13:43:00 +0700

    up

--- module 5/furama/src/app/app.module.ts
+++ module 5/furama/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderFooterNavComponent } from './header-footer-nav/header-footer-nav.component';
+import { ListServiceComponent } from './list-service/list-service.component';
+import { EditServiceComponent } from './edit-service/edit-service.component';
+import { AddServiceComponent } from './add-service/add-service.component';
+import { ListCustomerComponent } from './list-customer/list-customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderFooterNavComponent
+    HeaderFooterNavComponent,
+    ListServiceComponent,
+    EditServiceComponent,
+    AddServiceComponent,
+    ListCustomerComponent
   ],
   imports: [
     BrowserModule
